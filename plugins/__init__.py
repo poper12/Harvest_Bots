3b658cab2676e182ea2404b1000c36b241710f19
poper12 <samyakdhengle89@gmail.com>
2025-08-01 00:44:41 +0530

    __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -10,3 +10,6 @@ async def web_server():
     return web_app
 
 __all__ = ['admin_commands']
+
+#This repo is developed by @aaru_2075, don't you fucking dare to remove credit. [Ask @aaru_2075 before reselling it]
+#For Paid bot or support contact on @Manga_Campus_Chat
