bd7f7f147035108def0c6ac13cdfedb9cb06f020
poper12 <samyakdhengle89@gmail.com>
2025-08-01 00:44:57 +0530

    admin_commands.py

--- plugins/admin_commands.py
+++ plugins/admin_commands.py
@@ -1,3 +1,6 @@
+#This repo is developed by @aaru_2075, don't you fucking dare to remove credit. [Ask @aaru_2075 before reselling it]
+#For Paid bot or support contact on @Manga_Campus_Chat
+
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
