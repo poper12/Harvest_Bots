e6f1be1b47fb7a55926438122073fb467a207c45
poper12 <samyakdhengle89@gmail.com>
2025-08-01 00:48:49 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -114,8 +114,5 @@ async def delete_all_two():
 
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+#This repo is developed by @aaru_2075, don't you fucking dare to remove credit. [Ask @aaru_2075 before reselling it]
+#For Paid bot or support contact on @Manga_Campus_Chat
